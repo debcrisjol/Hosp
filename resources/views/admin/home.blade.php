bb0d98ef07c01c22c70867a21075518729059adb
debcrisjol <debcrisjol@gmail.com>
2023-02-15 03:44:43 +0000

    .

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+  <title>Medical Center Birmingham</title>
+
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/css/theme.css">
+</head>
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
@@ -50,10 +72,27 @@
         </div>
     </nav>
 
-    <main class="py-4">
-        @yield('content')
-    </main>
 </div>
-<h1>admin dashboard
-</h1>
+<h1>admin dashboard</h1>
+<p class="bg-warning">Qui sara il contenuto admin</p>
+<span>ci sei quasi</span>
+<footer class="page-footer">
+    <div class="container">
 
+
+      <p id="copyright">Copyright &copy; 2023 <a href="https://www.linkedin.com/in/cristinaandreeadanescu/" target="_blank">CRISTINA A.D.</a></p>
+    </div>
+  </footer>
+
+<script src="../assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/theme.js"></script>
+
+</body>
+</html>
